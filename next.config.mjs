d456e1bbf0e268b2dc6264b535aba9cfa718a4c5
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-08-11 20:50:55 +0200

    fix: vite

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  basePath: '/luca_bazzanella',
-  assetPrefix: '/luca_bazzanella/',
-  output: 'export',
-  trailingSlash: true, // recommended for static export
-  // ...other config...
-};
