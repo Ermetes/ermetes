e0ca10906208797ae3016e8b00ffa18c0293cac9
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-08-11 22:36:34 +0200

    fix: hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { ModalProvider } from "@/contexts/ModalContext";
 import Index from "./pages/Index";
@@ -21,7 +21,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/ermetes">
+          <HashRouter basename="/ermetes">
             <Routes>
               {/* Multilingual magazine article route */}
               <Route path="/magazine/:lang/:slug" element={<Article />} />
@@ -31,7 +31,7 @@ const App = () => (
 
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
           <PreventivoModal />
         </TooltipProvider>
       </ModalProvider>
