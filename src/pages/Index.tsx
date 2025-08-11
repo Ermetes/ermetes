69598934c03a94cae87e14212696821bb8c079ff
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-08-11 12:32:52 +0200

    feat: tabs

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { Button } from '@/components/ui/button';
 import InlineQuoteFormTrigger from "@/components/quote/InlineQuoteFormTrigger";
 import ConstructionBackground from "@/components/background/ConstructionBackground";
 import ModernNavigation from "@/components/navigation/ModernNavigation";
@@ -17,8 +18,25 @@ const Index = () => {
       <div className="relative z-10">
         <ModernNavigation />
       <ConstructionHero />
-      <div className="flex justify-center my-8">
-        <InlineQuoteFormTrigger />
+      <div className="flex justify-center my-8 gap-4">
+        <Button
+          onClick={() => {
+            const el = document.getElementById('services');
+            if (el) el.scrollIntoView({ behavior: 'smooth' });
+          }}
+          className="transition-all duration-300"
+        >
+          Edilizia Residenziale
+        </Button>
+        <Button
+          onClick={() => {
+            const el = document.getElementById('articles');
+            if (el) el.scrollIntoView({ behavior: 'smooth' });
+          }}
+          className="transition-all duration-300"
+        >
+          Articles
+        </Button>
       </div>
         <ServicesSection />
         <ProjectsScroll />
