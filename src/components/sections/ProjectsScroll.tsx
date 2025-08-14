e830ef0a7f1803a2859c81251741ccd6d857aa99
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-08-14 08:52:41 +0200

    fix: padding

--- src/components/sections/ProjectsScroll.tsx
+++ src/components/sections/ProjectsScroll.tsx
@@ -66,7 +66,7 @@ const ProjectsScroll = () => {
   return (
     <section id="projects">
       {/* Category Filter */}
-      <div className="bg-background sm:py-2 md:py-6 lg:sticky top-16 z-40 border-b border-border shadow-sm">
+      <div className="bg-background sm:py-2 lg:sticky top-16 z-40 border-b border-border shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center sm:mb-4 md:mb-8">
             <h2 className="text-2xl sm:text-3xl font-light text-foreground mb-4 mt-4">
