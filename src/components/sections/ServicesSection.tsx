88d88353125242469068be5dbd9faba73feeea15
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-09-02 22:32:22 +0200

    fix: margin

--- src/components/sections/ServicesSection.tsx
+++ src/components/sections/ServicesSection.tsx
@@ -96,7 +96,7 @@ const ServicesSection = () => {
         {/* Right: Second column with text block */}
         <div className="relative z-10 flex-1 flex flex-col justify-start items-center px-4 md:px-16 md:pl-8 py-12 pt-0 bg-transparent text-justify">
           <div className="w-full max-w-3xl mx-auto text-justify">
-            <h2 className="mt-2 text-4xl md:text-4xl text-left font-light mb-2 text-[#00338D] drop-shadow-lg">La missione di Ermetes</h2>
+            <h2 className="mt-2 text-4xl md:text-4xl text-left font-light mb-4 text-[#00338D] drop-shadow-lg">La missione di Ermetes</h2>
             <p className="text-gray-800 text-lg leading-relaxed text-justify [word-spacing:-1.5px] w-full font-extralight mb-2">La cooperativa ERMETES nasce proprio da questa visione: un percorso di trasformazione del “piombo” delle difficoltà quotidiane, all’“oro” di un gruppo coeso che condivide valori comuni generano benessere per gli individui e per la comunità.</p>
             <p className="text-gray-800 text-lg leading-relaxed text-justify [word-spacing:-1.5px] w-full font-extralight mb-2">Come l’alchimia ermetica trasmuta l’essere umano, così la nostra cooperativa aspira a trasformare le difficoltà quotidiane in opportunità di sviluppo e benessere collettivo.</p>
             <p className="text-gray-800 text-lg leading-relaxed text-justify [word-spacing:-1.5px] w-full font-extralight mb-2">Come ci insegna la storia di Ermete dunque se le opportunità vengono colte dai soci con etica e spirito costruttivo, si genera valore, soddisfazione e crescita per tutti. Se invece prevale lo spirito speculativo egoistico di pochi non ci potranno essere buoni frutti.</p>
